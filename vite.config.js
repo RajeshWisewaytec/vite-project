74465b4587f8d61bce5622379de0e409824f9d6e
RajeshWisewaytec <rajesh.kumar@wisewaytec.com>
2026-09-07 10:36:00 +0530

    feat: scaffold Vite project with initial configuration and assets

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import { defineConfig } from 'vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true, // Exposes server to network (http://192.168.1.11:5173)
+  },
 })
+
