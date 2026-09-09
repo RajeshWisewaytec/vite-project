cfed89a87ab38ad4fe1a32a14e11f2017d39350f
RajeshWisewaytec <rajesh.kumar@wisewaytec.com>
2026-09-09 10:24:15 +0530

    chore: set base path to /vite-project/ in vite configuration

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/vite-project/',
   server: {
     host: true, // Exposes server to network (http://192.168.1.11:5173)
   },
